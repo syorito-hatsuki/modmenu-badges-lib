510f8f25681c2191e60067a0694e250f61fd9260
syorito-hatsuki <enderdron3@gmail.com>
2023-09-30 03:28:11 +0300

    Init react files for late Github Page deploy

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+import "./App.css"
+
+export default function App() {
+    const [outlineColor, setOutlineColor] = useState("#000000")
+    const [backgroundColor, setBackgroundColor] = useState("#FFFFFF")
+
+    return (
+        <div className="container">
+        </div>
+    )
+}
