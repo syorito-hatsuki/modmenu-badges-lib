c06d7143d24de31fe8de3c2e0fe7f4a731b9cfa7
syorito-hatsuki <enderdron3@gmail.com>
2025-04-26 11:14:02 +0300

    Updated to 1.21.5 + Gradle cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
-import java.time.format.DateTimeFormatter
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 plugins {
     id("fabric-loom")
-    kotlin("jvm")
-    kotlin("plugin.serialization")
 }
 
 base {
@@ -12,9 +10,7 @@ base {
     archivesName.set(archivesBaseName)
 }
 
-val javaVersion = JavaVersion.VERSION_17
-val loaderVersion: String by project
-val minecraftVersion: String by project
+val javaVersion = JavaVersion.VERSION_21
 
 val modVersion: String by project
 version = "${DateTimeFormatter.ofPattern("yyyy.M").format(LocalDateTime.now())}.$modVersion"
@@ -23,33 +19,31 @@ val mavenGroup: String by project
 group = mavenGroup
 
 repositories {
-    maven("https://cursemaven.com")
     maven("https://api.modrinth.com/maven")
 }
 
 dependencies {
+    val minecraftVersion: String by project
     minecraft("com.mojang", "minecraft", minecraftVersion)
 
     val yarnMappings: String by project
     mappings("net.fabricmc", "yarn", yarnMappings, null, "v2")
 
+    val loaderVersion: String by project
     modImplementation("net.fabricmc", "fabric-loader", loaderVersion)
 
-    modImplementation("maven.modrinth", "modmenu", "RTFDnTKf")
+    val modmenuVersion: String by project
+    modImplementation("maven.modrinth", "modmenu", modmenuVersion)
 }
 
 tasks {
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        sourceCompatibility = javaVersion.toString()
-        targetCompatibility = javaVersion.toString()
-        options.release.set(javaVersion.toString().toInt())
-    }
-
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = javaVersion.toString()
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(javaVersion.toString()))
         }
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+        withSourcesJar()
     }
 
     jar {
@@ -59,23 +53,13 @@ tasks {
     processResources {
         inputs.property("version", project.version)
         filesMatching("fabric.mod.json") {
-            expand(
-                mutableMapOf(
-                    "version" to project.version,
-                    "loaderVersion" to loaderVersion,
-                    "minecraftVersion" to minecraftVersion,
-                    "javaVersion" to javaVersion.toString()
-                )
-            )
+            expand(mutableMapOf("version" to project.version))
         }
     }
-
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(javaVersion.toString()))
-        }
-        sourceCompatibility = javaVersion
-        targetCompatibility = javaVersion
-        withSourcesJar()
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        sourceCompatibility = javaVersion.toString()
+        targetCompatibility = javaVersion.toString()
+        options.release.set(javaVersion.toString().toInt())
     }
 }
